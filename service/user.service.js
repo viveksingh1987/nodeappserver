f506f063e100c3373b109a033a837bee9b366997
Vivek Singh <Vivek.Singh@datacom.co.nz>
2022-04-04 09:00:05 +1200

    updated

--- service/user.service.js
+++ service/user.service.js
@@ -1,8 +1,17 @@
 var mongoosedb = require("../db/mongoosedb");
 var UserModelSchema = require("../db/schema/user.model");
 
+/**
+ * 
+ * @param {*} userId 
+ * @returns 
+ */
 const findUser = async (userId) => {
   return await UserModelSchema.find({ userId: userId });
+};
+
+const addUser = async (userId) => {
+  return await UserModelSchema.save({ userId: userId });
 };
 
 module.exports = { findUser };
