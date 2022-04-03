6965c8145a27b9713908ac1b0f7f8fc9283fd9b2
Vivek Singh <viveksingh@Viveks-MBP-2.fritz.box>
2022-04-03 18:56:02 +1200

    updated

--- routes/user.routes.test.js
+++ routes/user.routes.test.js
@@ -1,4 +1,4 @@
-const request = require('supertest');
+const request = require("supertest");
 const express = require("express");
 const bodyParser = require("body-parser");
 const userRouter = require("./user.routes");
@@ -6,35 +6,30 @@ const userRouter = require("./user.routes");
 const app = new express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/user', userRouter);
+app.use("/user", userRouter);
 
-describe('Test User Routes', ()=>{
-    
-    it('Test user login failed with bad request', async () => {
-        await request(app)
-        .post('/user/login')
-        .then(res => {
-            expect(res.statusCode).toBe(400);    
-        }); 
-    });
+describe("Test User Routes", () => {
+  it("Test user login failed with bad request", async () => {
+    await request(app)
+      .post("/user/login")
+      .then((res) => {
+        expect(res.statusCode).toBe(400);
+      });
+  });
 
-    it('Test User is logged in failed and user not found', async () => {
-        await request(app)
-        .post('/user/login')
-        .send({userName: "vivek@gmail.com", password: "abc123"})
-        .then(res => {
-            expect(res.statusCode).toBe(401);    
-        });
-        
-    });
+  it("Test User is logged in failed and user not found", async () => {
+    await request(app)
+      .post("/user/login")
+      .send({ userName: "vivek@gmail.com", password: "abc123" })
+      .then((res) => {
+        expect(res.statusCode).toBe(401);
+      });
+  });
 
-    it('Test User is logged in with given credentials', async () => {
-        const res = await request(app)
-        .post('/user/login')
-        .send({userName: "vivek@gmail.com", password: "abc123"});
-        expect(res.statusCode).toBe(201);
-    });
-
-  
-
-})
+  it("Test User is logged in with given credentials", async () => {
+    const res = await request(app)
+      .post("/user/login")
+      .send({ userName: "vivek@gmail.com", password: "abc123" });
+    expect(res.statusCode).toBe(201);
+  });
+});
--- service/user.service.js
+++ service/user.service.js
@@ -1,8 +1,22 @@
 var mongoosedb = require("../db/mongoosedb");
 var UserModelSchema = require("../db/schema/user.model");
 
+/**
+ * Service to find user in Database
+ * @param {*} userId
+ * @returns
+ */
 const findUser = async (userId) => {
   return await UserModelSchema.find({ userId: userId });
 };
 
-module.exports = { findUser };
+/**
+ * Service to add user in Database
+ * @param {*} user
+ * @returns
+ */
+const addUser = async (user) => {
+  return await UserModelSchema.create(user);
+};
+
+module.exports = { findUser, addUser };
